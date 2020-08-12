5973461162d77b705552e579ba36f40090a7d92e
waltersanta1@hotmail.com <waltersanta1@hotmail.com>
2020-08-11 19:02:37 -0500

    add library project

---
+++ magic-strings/projects/magic-string/src/lib/magic-string.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nw-magic-string',
+  template: `
+    <p>
+      magic-string works!
+    </p>
+  `,
+  styles: []
+})
+export class MagicStringComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
